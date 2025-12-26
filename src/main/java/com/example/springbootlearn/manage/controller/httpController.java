b0dd882440881a0d37e38fbf34bdfc22d48ce7da
lihao <1679249763@qq.com>
2025-12-26 18:11:12 +0800

    day2

--- src/main/java/com/example/springbootlearn/manage/controller/httpController.java
+++ src/main/java/com/example/springbootlearn/manage/controller/httpController.java
@@ -1,6 +1,6 @@
 package com.example.springbootlearn.manage.controller;
 
-
+import com.alibaba.fastjson2.JSON;
 import com.alibaba.fastjson2.JSONObject;
 import com.example.springbootlearn.manage.vo.Result;
 import org.apache.hc.client5.http.classic.methods.HttpPost;
@@ -9,10 +9,11 @@ import org.apache.hc.client5.http.impl.classic.HttpClients;
 import org.apache.hc.core5.http.io.entity.StringEntity;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.*;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 @RequestMapping("/httpTo")
@@ -28,18 +29,39 @@ public class httpController {
         jsonObject.put("sex","男");
         jsonObject.put("address","上海");
         jsonObject.put("phone","12345678901");
-
-        try(CloseableHttpClient httpClient = HttpClients.createDefault()) {
-            HttpPost httpPost = new HttpPost("http://localhost:8080/test/test01");
-            httpPost.setEntity(new StringEntity(jsonObject.toString()));
-            logger.info("请求内容：{}", httpPost.toString());
-            httpClient.execute(httpPost, response ->{
-                logger.info("响应状态：{}", response.getCode());
-                logger.info("响应内容：{}", response.getEntity().getContent().toString());
-                return null;
-            });
+        try{
+            HttpHeaders httpHeaders = new HttpHeaders();
+            httpHeaders.setContentType(MediaType.APPLICATION_JSON);
+            httpHeaders.set("Accept-Charset", "UTF-8");
+            HttpEntity<JSONObject> jsonObjectHttpEntity = new HttpEntity<>(jsonObject, httpHeaders);
+            RestTemplate restTemplate = new RestTemplate();
+            ResponseEntity<String> response = restTemplate.exchange("http://localhost:8080/test/test01", HttpMethod.POST, jsonObjectHttpEntity, String.class);
+            logger.info("响应状态：{}", response.getStatusCode());
+            logger.info("响应内容：{}", response.getBody());
+            JSONObject repJson = JSON.parseObject(response.getBody());
+            logger.info("repJson-code：{}", repJson.get("code"));
+            logger.info("codeIndex:{}", response.getBody().toString().indexOf("code"));
+            logger.info("code:{}", response.toString().substring(response.toString().indexOf("code")+6, response.toString().indexOf("code")+7));
+        } catch(Exception e) {
+            logger.info("异常：{}", e.getMessage());
         }
-        catch(Exception e) {
+        return Result.ok();
+    }
+
+    @GetMapping("test02")
+    public Result test02()
+    {
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("name","张三");
+        jsonObject.put("age",18);
+        jsonObject.put("sex","男");
+        jsonObject.put("address","上海");
+        jsonObject.put("phone","12345678901");
+        try(CloseableHttpClient client = HttpClients.createDefault()){
+            HttpPost httpPost = new HttpPost("http://localhost:8080/test/test01");
+            httpPost.setEntity(new StringEntity("{\"key\":\"value\"}"));
+
+        } catch(Exception e) {
             logger.info("异常：{}", e.getMessage());
         }
         return Result.ok();
--- src/main/java/com/example/springbootlearn/manage/controller/testController.java
+++ src/main/java/com/example/springbootlearn/manage/controller/testController.java
@@ -1,6 +1,7 @@
 package com.example.springbootlearn.manage.controller;
 
 
+import com.alibaba.fastjson2.JSON;
 import com.alibaba.fastjson2.JSONObject;
 import com.example.springbootlearn.manage.vo.Result;
 import com.example.springbootlearn.manage.vo.test01Vo;
@@ -16,7 +17,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class testController {
     private static final Logger logger = LoggerFactory.getLogger(testController.class);
 
-
     @PostMapping("/test01")
     public Result test01(@RequestBody test01Vo vo)
     {
@@ -26,8 +26,30 @@ public class testController {
         jsonObject.put("sex","男");
         jsonObject.put("address","上海");
         jsonObject.put("phone","12345678901");
-        logger.info("jsonObject：{}", jsonObject.toString());
+        logger.info("jsonObject：{}", jsonObject);
         logger.info("vo：{}", vo.toString());
-        return Result.ok();
+        return Result.ok(jsonObject);
+    }
+
+    @PostMapping("/test02")
+    public Result test02(@RequestBody String str)
+    {
+        try{
+            logger.info("str：{}", str);
+            JSONObject jsonObject = JSON.parseObject(str);
+            if(jsonObject.get("name")!= null && jsonObject.get("name") != "")
+            {
+                logger.info("name：{}", jsonObject.get("name"));
+            }
+            if(!jsonObject.isEmpty() && jsonObject != null)
+            {
+                logger.info("jsonObject：{}", jsonObject);
+            }
+            return Result.ok(jsonObject);
+        }
+        catch(Exception e) {
+            logger.info("异常：{}", e.getMessage());
+        }
+        return Result.error("error");
     }
 }
